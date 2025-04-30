c7d9eee07d2b953aad6f420a9623553d95cf1bbc
Ivan Strel'nikov <ivanstrel87@gmail.com>
2025-04-30 22:54:45 +0300

    Update assets.js, fix bug with required rootpath in cache

--- server/models/assets.js
+++ server/models/assets.js
@@ -201,7 +201,11 @@ module.exports = class Asset extends Model {
     }
     const sendFile = Promise.promisify(res.sendFile, {context: res})
     res.type(path.extname(assetPath))
-    await sendFile(cachePath, { dotfiles: 'deny' })
+    try {
+      await sendFile(cachePath, { dotfiles: 'deny' })
+    } catch (error) {
+          await sendFile(cachePath, { dotfiles: 'deny', root: WIKI.ROOTPATH })
+    }
     return true
   }
 
